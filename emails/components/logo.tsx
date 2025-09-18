d0f6e4a459ad4acbdf12ef94d7ef68ab37d4fbd3
winson-tang <winsontang@gmail.com>
2025-09-18 14:22:48 -0700

    initial commit

--- emails/components/logo.tsx
+++ emails/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo(props: { className?: string }) {
   return (
     <Img
       className={props.className}
-      src={'../../static/logo.png'}
+      src={'https://lwjgsigutkkuxolfsxui.supabase.co/storage/v1/object/public/public-assets/assets/logo.png'}
       width={80}
     />
   );
